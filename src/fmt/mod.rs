16ed7e08404bfd572cd029726ddae2649f0d33f0
Tabitha Rollins <tabitha@keyneston.com>
2019-07-17 17:06:06 +0100

    Function to parse intro line of a graphql query

--- src/fmt/mod.rs
+++ src/fmt/mod.rs
@@ -2,8 +2,11 @@ extern crate nom;
 use std::fs;
 
 use nom::{
+    branch::alt,
     bytes::complete::{tag, take_while_m_n},
-    combinator::map_res,
+    character::complete::{char, space0},
+    combinator::{map_res, opt, peek},
+    error::make_error,
     sequence::tuple,
     IResult,
 };
@@ -39,10 +42,55 @@ pub fn format_string(input: &str) -> String {
 }
 
 fn parse_query(input: &str) -> IResult<&str, GraphqlQuery> {
+    let (input, queryType) = parse_query_type(input)?;
+
     Ok((
         input,
         GraphqlQuery {
-            queryType: QueryType::Query,
+            queryType: queryType,
         },
     ))
+}
+
+fn parse_query_type(input: &str) -> IResult<&str, QueryType> {
+    let (input, _) = space0(input)?;
+    let (input, queryType) = opt(alt((tag("query"), tag("mutation"))))(input)?;
+    let (input, _) = space0(input)?;
+    let (input, _) = char('{')(input)?;
+
+    match queryType {
+        Some("query") => Ok((input, QueryType::Query)),
+        Some("mutation") => Ok((input, QueryType::Mutation)),
+        Some(queryType) => unimplemented!(),
+        None => Ok((input, QueryType::Query)),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn mutation_parse_query_type() {
+        let input = "mutation {";
+        let (_, queryType) = parse_query_type(input).unwrap();
+
+        assert_eq!(queryType, QueryType::Mutation);
+    }
+
+    #[test]
+    fn query_parse_query_type() {
+        let input = "query {";
+        let (_, queryType) = parse_query_type(input).unwrap();
+
+        assert_eq!(queryType, QueryType::Query);
+    }
+
+    fn default_parse_query_type() {
+        let input = "{";
+        let (_, queryType) = parse_query_type(input).unwrap();
+
+        assert_eq!(queryType, QueryType::Query);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 extern crate clap;
+
 use clap::{App, Arg, ArgMatches, SubCommand};
 use std::fs;
 
